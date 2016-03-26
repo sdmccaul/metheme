ade9bfa649fbbf86e168cc23b1caa3f1d837b5cf
Steven McCauley <steven_mccauley@brown.edu>
2016-03-26 17:43:32 -0400

    Fix array syntax

--- wp-content/themes/metheme/functions.php
+++ wp-content/themes/metheme/functions.php
@@ -167,19 +167,19 @@ require get_template_directory() . '/inc/customizer.php';
 require get_template_directory() . '/inc/jetpack.php';
 
 function custom_post_feed_init() {
+	$labels = array(
+		'name' => 'Feed Data',
+		'singular_name' => 'Feed Post',
+		'add_new' => 'Add New',
+		'add_new_item' => 'Add New Feed Post',
+		'edit_item' => 'Edit Feed Post',
+		'new_item' => 'New Feed Post',
+		'view_item' => 'View Feed Post',
+		'search_items' => 'Search Feed Data',
+		'not_found' => 'No feed data found',
+		'not_found_in_trash' => 'No feed data found in trash'
+	);
 	$args = array(
-		$labels = array(
-			'name' => 'Feed Data',
-			'singular_name' => 'Feed Post',
-			'add_new' => 'Add New',
-			'add_new_item' => 'Add New Feed Post',
-			'edit_item' => 'Edit Feed Post',
-			'new_item' => 'New Feed Post',
-			'view_item' => 'View Feed Post',
-			'search_item' => 'Search Feed Data',
-			'not_found' => 'No feed data found',
-			'not_found_in_trash' => 'No feed data found in trash'
-		);
 		'labels'			=> $labels,
 		'description'		=> 'Data from external feeds',
 		'public'			=> true,
@@ -187,7 +187,7 @@ function custom_post_feed_init() {
 		'has_archive'		=> true,
 		'menu_icon'			=> 'dashicons-controls-repeat',
 		'menu_position'		=> 5,
-		'supports'			=> array( 'title','custom-fields'),
+		'supports'			=> array( 'title','custom-fields')
 	);
 	register_post_type( 'feed-data', $args );
 	flush_rewrite_rules();
