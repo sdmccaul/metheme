1d37bd400b6856281c5f18fe6778c830479417e4
Steven McCauley <steven_mccauley@brown.edu>
2016-03-26 17:18:45 -0400

    update custom feed post

--- wp-content/themes/metheme/functions.php
+++ wp-content/themes/metheme/functions.php
@@ -166,21 +166,30 @@ require get_template_directory() . '/inc/customizer.php';
  */
 require get_template_directory() . '/inc/jetpack.php';
 
-function custom_post_feed() {
+function custom_post_feed_init() {
 	$args = array(
-		'labels'			=> 'Feed data',
+		$labels = array(
+			'name' => 'Feed Data',
+			'singular_name' => 'Feed Post',
+			'add_new' => 'Add New',
+			'add_new_item' => 'Add New Feed Post',
+			'edit_item' => 'Edit Feed Post',
+			'new_item' => 'New Feed Post',
+			'view_item' => 'View Feed Post',
+			'search_item' => 'Search Feed Data',
+			'not_found' => 'No feed data found',
+			'not_found_in_trash' => 'No feed data found in trash'
+		);
+		'labels'			=> $labels,
 		'description'		=> 'Data from external feeds',
 		'public'			=> true,
-		'show_ui'			=> true,
-		'capability_type'	=> 'post',
-		'hierarchical'		=> false,
 		'rewrite'			=> array('slug' => 'feed-data'),
-		'query_var'			=> true,
+		'has_archive'		=> true,
 		'menu_icon'			=> 'dashicons-controls-repeat',
 		'menu_position'		=> 5,
-		'supports'			=> array( 'title', 'editor', 'thumbnail', 'excerpt'),
-		'has_archive'		=> true,
+		'supports'			=> array( 'title','custom-fields'),
 	);
-	register_post_type( 'feed-data', $args ); 
+	register_post_type( 'feed-data', $args );
+	flush_rewrite_rules();
 }
-add_action( 'init', 'custom_post_feed' );
+add_action( 'init', 'custom_post_feed_init' );
