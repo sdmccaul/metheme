3a2515fd5bc19b30d203bee633728cff0c3cb25a
Steven McCauley <steven_mccauley@brown.edu>
2016-04-02 10:12:08 -0400

    Get custom field to display in editor

--- wp-content/plugins/metheme-plugin/metheme-plugin.php
+++ wp-content/plugins/metheme-plugin/metheme-plugin.php
@@ -25,12 +25,38 @@ function custom_post_feed_init() {
 		'has_archive'		=> true,
 		'menu_icon'			=> 'dashicons-controls-repeat',
 		'menu_position'		=> 5,
-		'supports'			=> array('title','excerpt','custom-fields')
+		'supports'			=> array('title','excerpt','custom-fields'),
+		'register_meta_box_cb' => 'add_feed_data_metaboxes',
 	);
 	register_post_type( 'feed-data', $args );
 	flush_rewrite_rules();
 }
+
+function add_feed_data_metaboxes() {
+	add_meta_box('feed_data_source', 'Data Source', 'feed_data_source', 'feed-data', 'normal', 'default');
+}
+
+// The Event Location Metabox
+
+function feed_data_source() {
+	global $post;
+	
+	// Noncename needed to verify where the data originated
+	echo '<input type="hidden" name="feedmeta_noncename" id="feedmeta_noncename" value="' . 
+	wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+	
+	// Get the location data if its already been entered
+	$source = get_post_meta($post->ID, '_source', true);
+	
+	// Echo out the field
+	echo '<input type="text" name="_source" value="' . $source  . '" class="widefat" />';
+
+}
+
+
 add_action( 'init', 'custom_post_feed_init' );
+
+// add_action( 'add_meta_boxes', 'add_feed_data_metaboxes');
 
 /* Stop Adding Functions Below this Line */
 ?>
